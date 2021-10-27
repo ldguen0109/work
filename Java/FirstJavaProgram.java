f539bdf9e364c4eee7b2656cbedcbf2280a423f6
ldguen0109 <ldguen0109@gmail.com>
2021-10-27 12:49:16 +0900

    p33 문제01-1

---
+++ Java/FirstJavaProgram.java
@@ -0,0 +1,6 @@
+class SimpleJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("First Java program");
+    }
+}
---
+++ Java/problem2.java
@@ -0,0 +1,6 @@
+public class problem2 {
+    public static void main(String[] args) {
+        System.out.println("2 + 5 = " + 2 + 5);
+        System.out.println("2 + 5 = " + (2 + 5));
+    }
+}
