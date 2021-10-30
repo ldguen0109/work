d2ff05037a3c8e1d78354154ddcb9ef936c824fd
ldguen0109 <ldguen0109@gmail.com>
2021-10-30 21:37:47 +0900

    2021-10-30

--- Java/example.java
+++ Java/example.java
@@ -1,12 +1,8 @@
 class UseVariable {
     public static void main(String[] args) {
-        int num1; 
-        num1 = 10;
-
-        int num2;
-        num2 = 20;
-
-        int num3 = num1 + num2;
-        System.out.println(num1 + " + " + num2 + " = " + num3);
+        short num1 = 11;
+        short num2 = 22;
+        int result = num1 + num2;
+        System.out.println(result);
     }
 }
