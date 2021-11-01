a7bba3d1d884d89ba5e9ebb15fb25de76f20d615
ldguen0109 <ldguen0109@gmail.com>
2021-11-01 20:13:33 +0900

    2021-11-01

--- Java/example.java
+++ Java/example.java
@@ -1,11 +1,11 @@
 class Constants {
     public static void main(String[] args) {
-        final int MAX_SIZE = 100;
-        final char CONST_CHAR = '상';
-        final int CONST_ASSIGNED;
-        CONST_ASSIGNED = 12;
-        System.out.println("상수1 : " + MAX_SIZE);
-        System.out.println("상수2 : " + CONST_CHAR);
-        System.out.println("상수3 : " + CONST_ASSIGNED);
+        System.out.println("AB" + '\b' + 'C');
+        System.out.println("AB" + '\t' + 'C');
+        System.out.println("AB" + '\\' + 'C');
+        System.out.println("AB" + '\'' + 'C');
+        System.out.println("AB" + '\"' + 'C');
+        System.out.println("AB" + '\n' + 'C');
+        System.out.println("AB" + '\r' + 'C');
     }
 }
