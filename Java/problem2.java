d1ecf7d0ef1e66393e7ae45028f304a6513e8ba9
ldguen0109 <ldguen0109@gmail.com>
2021-11-07 20:52:09 +0900

    2021-11-07

--- Java/problem2.java
+++ Java/problem2.java
@@ -1,20 +1,8 @@
 class SCE { 
     public static void main(String[] args) {
-        int num1 = 0;
-        int num2 = 0;
-        boolean result;
-        num1 += 10;
-        num2 += 10;
-        result = (num1 < 0) && (num2 > 0);
-        System.out.println("result = " + result);
-        System.out.println("num1 = " + num1);
-        System.out.println("num2 = " + num2 + '\n');
+        int num = 15678;
+        System.out.println((num>>2) & 1);
+        System.out.println( (num>>4) & 1);
 
-        num1 += 10;
-        num2 += 10;
-        result = (num1 > 0) || (num2 > 0);
-        System.out.println("result = " + result);
-        System.out.println("num1 = " + num1);
-        System.out.println("num2 = " + num2);
     }    
 }
--- Java/problem3.java
+++
@@ -1,8 +0,0 @@
-class problem3 { 
-    public static void main(String[] args) {
-        double result = ((25 * 5) + (36-4)-72) / 5;
-        double result1 = (double)((25 * 5) + (36-4)-72) / 5;
-        System.out.println("result : " + result);
-        System.out.println("result1 : " + result1);
-    }    
-}
